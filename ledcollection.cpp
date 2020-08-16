096eb4c6b48266e33a1137fdfcd6eb65622881d5
David Adamson <36890442+adamsondavid@users.noreply.github.com>
2020-08-16 13:33:48 +0200

    init vledstrip indicies

--- ledcollection.cpp
+++ ledcollection.cpp
@@ -7,12 +7,12 @@
 #include "alrbtmappingstrategy.h"
 
 LEDCollection::LEDCollection(){
-  // TODO: init the strips properly
-  this->physical_strip = new Adafruit_NeoPixel(10, 5, NEO_GRB + NEO_KHZ800);
-  this->ledStrip_front  = new LEDStripMirror(new VLEDStrip(physical_strip, 0, 1), new VLEDStrip(physical_strip, 0, 1));
-  this->ledStrip_back   = new LEDStripMirror(new VLEDStrip(physical_strip, 0, 1), new VLEDStrip(physical_strip, 0, 1));
-  this->ledStrip_bottom = new LEDStripMirror(new VLEDStrip(physical_strip, 0, 1), new VLEDStrip(physical_strip, 0, 1));
-  this->ledStripMatrix  = new LEDStripMatrixMirror(new LEDStripMatrix(13, 8, new VLEDStrip(physical_strip, 0, 1), new ALRBTMappingStrategy()), new LEDStripMatrix(13, 8, new VLEDStrip(physical_strip, 0, 1), new ALRBTMappingStrategy()));
+  this->physical_strip = new Adafruit_NeoPixel(314, 5, NEO_GRB + NEO_KHZ800);
+  
+  this->ledStrip_front  = new LEDStripMirror(new VLEDStrip(physical_strip, 304, 296), new VLEDStrip(physical_strip, 305, 313));
+  this->ledStrip_back   = new LEDStripMirror(new VLEDStrip(physical_strip, 191, 183), new VLEDStrip(physical_strip, 174, 182));
+  this->ledStrip_bottom = new LEDStripMirror(new VLEDStrip(physical_strip, 0, 34), new VLEDStrip(physical_strip, 69, 35));
+  this->ledStripMatrix  = new LEDStripMatrixMirror(new LEDStripMatrix(13, 8, new VLEDStrip(physical_strip, 192, 295), new ALRBTMappingStrategy()), new LEDStripMatrix(13, 8, new VLEDStrip(physical_strip, 70, 173), new ALRBTMappingStrategy()));
 }
 
 void LEDCollection::begin(){
